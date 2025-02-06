7c92c11f29204bd40e9d89a95490e495dc577a61
LoicMDF <60476871+loicmdf@users.noreply.github.com>
2025-02-06 16:19:50 +0400

    Update loaders.js

--- loaders.js
+++ loaders.js
@@ -21,13 +21,14 @@ function LoadFooter() {
 }
 async function writeBook(book) {
     document.getElementsByClassName("book-list").item(0).insertAdjacentHTML("beforeend", `
-        <button title="${book.title}\nAuthor: ${book.author}\nPublished: ${book.year}\nPrice: $${book.price}" id="${book.id}" style="border: 2px solid #000;
+        <button class="bookItem" title="${book.title}\nAuthor: ${book.author}\nPublished: ${book.year}\nPrice: $${book.price}" id="${book.id}" style="border: 2px solid #000;
                 border-radius: 5px;
                 cursor: pointer;"
+                transition: 0.1s;
                 onclick="window.location.href = '/temblemy/delivery.html?id=${book.id}'"
                 onmouseover="this.style.border = '2px solid #FFF'"
                 onmouseout="this.style.border = '2px solid #000'">
-            <div class="bookItem" style="
+            <div class="bookItemImg" style="
                  width: 250px;
                  height: 350px;
                  color: white;
